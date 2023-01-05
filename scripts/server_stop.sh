3feca7344f238ee8666fe70d9740aa14f57855a5
KSLeeDev <107389012+KSLeeDev@users.noreply.github.com>
2023-01-05 16:03:52 +0900

    Update server_stop.sh

--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo rm -rf /home/ec2-user/tomcat/webapps/ROOT/insert.jsp
-sudo rm -rf /home/ec2-user/tomcat/webapps/ROOT/insertPro.jsp
-sudo rm -rf /home/ec2-user/tomcat/webapps/ROOT/view.jsp
+#sudo rm -rf /home/ec2-user/tomcat/webapps/ROOT/insert.jsp
+#sudo rm -rf /home/ec2-user/tomcat/webapps/ROOT/insertPro.jsp
+#sudo rm -rf /home/ec2-user/tomcat/webapps/ROOT/view.jsp
 sudo chmod +x /home/ec2-user/tomcat/bin/./shutdown.sh
 sudo /home/ec2-user/tomcat/bin/./shutdown.sh
 echo 1
